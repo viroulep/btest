20cb9d5500c14eda65253392e905b10506413c15
Philippe Virouleau <philippe.44@gmail.com>
2020-11-24 23:00:43 +0100

    nginx

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -34,10 +34,11 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # FIXME: get this from env
+  config.action_cable.allowed_request_origins = [ 'https://quizz.virouleau.fr' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
