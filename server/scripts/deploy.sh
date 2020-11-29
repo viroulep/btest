484cdec551b5e3ca5652580d561e1a67648e3508
Philippe Virouleau <philippe.44@gmail.com>
2020-11-29 14:50:37 +0100

    Update starting puma

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+pull_latest() {
+  git pull
+}
+
 restart_app() {
   if ps -efw | grep "puma" | grep -v grep; then
-    echo "Restarting puma"
     # Found a puma process, restart it gracefully
     pid=$(<"/tmp/puma.pid")
     kill -SIGUSR2 $pid
   else
     # We could not find a puma master process running, lets start one up!
-    bundle exec puma &
+    nohup bundle exec puma &
   fi
 }
 
-deploy_latest() {
-  git pull
+deploy() {
   sudo chef-solo -o 'role[btest-app]' -E production -c ../ci/chef/solo.rb
   set +x
   # Re-source env in case stuff changed
@@ -24,5 +26,5 @@ deploy_latest() {
 
 cd "$(dirname "$0")"/..
 
-allowed_commands="restart_app deploy_latest"
+allowed_commands="restart_app deploy pull_latest"
 source scripts/_parse_args.sh
