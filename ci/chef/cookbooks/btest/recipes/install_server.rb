8da9c7afe78fb48fdccbe25a4c9edaf1de29f8ee
Philippe Virouleau <philippe.44@gmail.com>
2020-11-29 15:13:58 +0100

    update bundle config

--- ci/chef/cookbooks/btest/recipes/install_server.rb
+++ ci/chef/cookbooks/btest/recipes/install_server.rb
@@ -1,11 +1,11 @@
-# Make sure the environment makes sense
-rails_env = node.chef_environment == "production" ? "production" : "development"
-
-extra_args = "--without development test" if rails_env == "production"
-
 # FIXME: it would be nice to start the server somewhere here
 #"bin/rails s -p 8080 -b 0.0.0.0 &"
 #ps -efw |grep "puma"|grep -v grep|tr -s ' ' |cut -f 2 -d " "
+template "/home/btest/btest/server/.bundle/config" do
+  source "bundle.erb"
+  owner "btest"
+  group "btest"
+end
 
 # FIXME: the environment is also set in envrc.
 # It would be nice to check the consistency, and abort if inconsistent!
@@ -13,9 +13,10 @@ bash "run the server actions" do
   cwd "/home/btest/btest/server"
   code <<-EOF
     source /home/btest/btest/env/envrc
-    bundle install --path vendor/bundle -j3 #{extra_args}
+    bundle install
     bin/rails db:create
     bin/rails db:migrate
   EOF
+  environment ({ "HOME": "/home/btest" })
   user "btest"
 end
